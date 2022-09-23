d251f12a9b72ac54e3cd5854f82434ccefea6bfe
auroraMoreno <shiningbrighteraml13@gmail.com>
2022-09-23 19:17:22 +0200

    strncmp subido

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,5 +1,27 @@
 #include "libft.h"
+#include <stdio.h>
+#include <string.h>
+
 int ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	
+	unsigned int i;
+
+    i = 0;
+    while((s1)[i] != '\0' && s2[i] != '\0' && i < n)
+    {
+        if(((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
+            return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+        i++;
+    }
+
+    if(i != n)
+        return(((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+    
+    return (0);
+}
+
+int main(void)
+{
+    printf("%d", ft_strncmp("hola que tal", "hola que haces", 7));
+    printf("%d", strncmp("hola que tal", "hola que haces", 7));
 }
