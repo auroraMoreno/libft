ce3eae2e9431f9a287d76acf1622e8ca6e9ddc42
auroraMoreno <shiningbrighteraml13@gmail.com>
2022-09-23 19:50:39 +0200

    memchr hecha

--- ft_memchr.c
+++ ft_memchr.c
@@ -10,8 +10,33 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include <string.h>
+#include <stdio.h>
 
 void *ft_memchr(const void *s, int c, size_t n)
 {
+    size_t i;
+    unsigned char *str = (unsigned char *)s;
+    unsigned char j = (unsigned char)c;
 
+    i = 0; 
+    while(i < n)
+    {
+        if(str[i] == j)
+            return ((void *)str + i);
+        i++; 
+    }
+
+    return (0);
 }
+
+/*
+int main(void)
+{
+    //devuelve el resto del string despues de 'o' 
+    char *c = memchr("hola que tal", 'u', 7);
+    char *ft_c = ft_memchr("hola que tal", 'u', 7);
+    printf("%s", ft_c);
+    printf("%s", c);
+}
+*/
