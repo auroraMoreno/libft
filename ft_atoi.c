c8617c79741a8519d98cf8f8aecb27697c88da21
Aurora Moreno lopez <aumoreno@c1r14s4.42madrid.com>
2022-12-21 09:46:56 +0100

    ult

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,35 +6,40 @@
 /*   By: aumoreno <aumoreno@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 10:46:26 by aumoreno          #+#    #+#             */
-/*   Updated: 2022/09/25 10:46:26 by aumoreno         ###   ########.fr       */
+/*   Updated: 2022/12/21 08:10:56 by aumoreno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-int ft_atoi(const char *str)
+//#include "stdio.h"
+int	ft_atoi(const char *str)
 {
-    int res;
-    int sign;
+	int	res;
+	int	sign;
 
-    res = 0;
-    sign = 1;
-
-    while((*str >= 9 && *str <= 13) || (*str == ' '))
-    {
-        str++;
-    }
-    if(*str == '+')
-        str++;
-    if(*str == '-')
-    {
-        sign *= -1;
-        str++;
-    }
-    while(*str >= '0' && *str <= '9')
-    {
-        res = res * 10 + *str - '0';
-        str++;
-    }
-    return(res * sign);
+	res = 0;
+	sign = 1;
+	while ((*str >= 9 && *str <= 13) || (*str == ' '))
+	{
+		str++;
+	}
+	if (*str == '+')
+		str++;
+	if (*str == '-')
+	{
+		sign *= -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		res = res * 10 + *str - '0';
+		str++;
+	}
+	return (res * sign);
 }
+/*
+int main (void)
+{
+	printf("%u", ft_atoi("234ABC3"));
+}
+*/
--- ft_isalpha.c
+++ ft_isalpha.c
@@ -3,22 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isalpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aumoreno <aumoreno@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: aumoreno <aumoreno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 10:41:16 by aumoreno          #+#    #+#             */
-/*   Updated: 2022/09/25 10:41:16 by aumoreno         ###   ########.fr       */
+/*   Updated: 2022/12/21 09:45:32 by aumoreno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //#include <stdio.h>
-
-int ft_isalpha(int c)
+int	ft_isalpha(int c)
 {
-    if((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+    if  ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
     {
         return (1);
-    }
 
+    }
     return (0);
 }
 /*
